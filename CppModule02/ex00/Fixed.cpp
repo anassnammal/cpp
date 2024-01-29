023b19d9500cae5e9e02be33a4d67ae2febfba42
anammal <anammal@student.1337.ma>
2024-01-29 19:26:10 +0100

    at ex00

---
+++ CppModule02/ex00/Fixed.cpp
@@ -0,0 +1,16 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+
+}
+
+Fixed::Fixed(Fixed &tmp)
+{
+	*this = tmp;
+}
+
+Fixed::~Fixed(void)
+{
+
+}
---
+++ CppModule02/ex00/Fixed.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	/* data */
+public:
+	Fixed(void);
+	Fixed(Fixed &tmp);
+
+	~Fixed(void);
+};
+
