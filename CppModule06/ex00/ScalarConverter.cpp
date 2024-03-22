65524846974c487f1b85e486262ac2348171af52
Anass Nammal <anammal@student.1337.ma>
2024-03-22 00:25:30 +0000

    done

--- CppModule06/ex00/ScalarConverter.cpp
+++ CppModule06/ex00/ScalarConverter.cpp
@@ -14,6 +14,7 @@ void ScalarConverter::convert(std::string const s)
         char c = s.at(0);
         std::cout << "char: " << (std::isprint(c) ? ('\'' + std::string(1, c) + '\'') : ("not displayable")) << std::endl;
         std::cout << "int: " << static_cast<int>(c) << std::endl;
+        std::cout << std::fixed << std::setprecision(1);
         std::cout << "float: " << static_cast<float>(c) << 'f' << std::endl;
         std::cout << "double: " << static_cast<double>(c) << std::endl;
     }
@@ -22,16 +23,20 @@ void ScalarConverter::convert(std::string const s)
         if (s.find('f') != s.npos)
         {
             float f = static_cast<float>((atof(s.c_str())));
-            std::cout << "char: " << (std::isprint(static_cast<int>(f)) ? ('\'' + std::string(1, static_cast<int>(f)) + '\'') : ("not displayable")) << std::endl;
+            std::cout << "char: " << (std::isprint(static_cast<char>(f)) ? ('\'' + std::string(1, static_cast<char>(f)) + '\'') : ("not displayable")) << std::endl;
             std::cout << "int: " << static_cast<int>(f) << std::endl;
+            if (f == floor(f))
+                std::cout << std::fixed << std::setprecision(1);
             std::cout << "float: " << f << 'f' << std::endl;
             std::cout << "double: " << static_cast<double>(f) << std::endl;
         }
         else
         {
             double d = static_cast<double>((atof(s.c_str())));
-            std::cout << "char: " << (std::isprint(static_cast<int>(d)) ? ('\'' + std::string(1, static_cast<int>(d)) + '\'') : ("not displayable")) << std::endl;
+            std::cout << "char: " << (std::isprint(static_cast<char>(d)) ? ('\'' + std::string(1, static_cast<char>(d)) + '\'') : ("not displayable")) << std::endl;
             std::cout << "int: " << static_cast<int>(d) << std::endl;
+            if (d == floor(d))
+                std::cout << std::fixed << std::setprecision(1);
             std::cout << "float: " << static_cast<float>(d) << 'f' << std::endl;
             std::cout << "double: " << d << std::endl;
         }
@@ -41,6 +46,7 @@ void ScalarConverter::convert(std::string const s)
         int i = atoi(s.c_str());
         std::cout << "char: " << (std::isprint(i) ? ('\'' + std::string(1, static_cast<char>(i)) + '\'') : ("not displayable")) << std::endl;
         std::cout << "int: " << i << std::endl;
+        std::cout << std::fixed << std::setprecision(1);
         std::cout << "float: " << static_cast<float>(i) << 'f' << std::endl;
         std::cout << "double: " << static_cast<double>(i) << std::endl;
     }
